5c95ef46272c44f6ff456578673aa86cd272ca09
gravito <aayush.shrut@gmail.com>
2019-01-07 11:03:23 +0530

    Final changes

--- Numbers/pi.py
+++ Numbers/pi.py
@@ -1,8 +1,10 @@
 from math import pi
 if __name__ == "__main__":
     while True:
-        n = int(input("Please enter positive value \n"))
+        n = int(input("Please enter positive value below 48 \n"))
         try:
+            if n > 48:
+                raise Exception("Please enter below 48 \n")
             if n <= 0:
                 raise Exception("Please enter positive value \n")
             break
